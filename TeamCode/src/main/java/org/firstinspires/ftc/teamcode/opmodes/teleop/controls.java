395c961cda4a5123b25d26659289be75a6fbe2c9
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-02-10 00:48:37 -0600

    final push of the night

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/controls.java
@@ -14,21 +14,30 @@ public class controls {
         robot.movement.odo.bulkUpdate();
         robot.movement.move(op.gamepad1.left_stick_x, op.gamepad1.left_stick_y, op.gamepad1.right_stick_x);
         if (op.gamepad1.right_trigger > 0.3) {
-            robot.intake.takeIn();
+            robot.arm.afterSpecimen();
         }
-        else if (op.gamepad1.left_trigger > 0.3) {
+        if (op.gamepad1.left_trigger > 0.3) {
             robot.intake.eject();
         }else{
             robot.intake.stop();
         }
-        if(op.gamepad1.right_bumper && op.gamepad1.left_bumper){
-            robot.arm.afterSpecimen();
-        }
-        else if(op.gamepad1.right_bumper){
+        if(op.gamepad1.right_bumper){
             robot.arm.getSpecimen();
         }
-        else if(op.gamepad1.left_bumper){
+        if(op.gamepad1.left_bumper){
             robot.arm.placeSpecimen();
+        }
+        if(op.gamepad1.y){
+            robot.arm.outTank();
+        }
+        if(op.gamepad1.b){
+            robot.arm.inTank();
+        }
+        if(op.gamepad1.a){
+            robot.arm.highBasket();
+        }
+        if(op.gamepad1.x){
+            robot.arm.hang();
         }
     }
 }
