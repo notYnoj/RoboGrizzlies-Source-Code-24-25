c484c994a46753bb9b5d27f3aa508553a8b3ad81
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-02-09 22:54:36 -0600

    ac

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/arm/arm.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.mech.arm;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+public class arm {
+    public static double high_basket;
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mech/intake/intake.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.mech.intake;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+public class intake {
+    CRServo active_intake;
+    public static double intake_constant = 1.0;
+    public intake(LinearOpMode l){
+        l.hardwareMap.get(Servo.class, "active_intake");
+    }
+    public void intake(){
+        active_intake.setPower(intake_constant);
+    }
+    public void eject(){
+        active_intake.setPower(-intake_constant);
+    }
+    public void stop(){
+        active_intake.setPower(0);
+    }
+}
